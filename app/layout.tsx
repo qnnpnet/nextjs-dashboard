a90fa426aa6fcd3cd72d9817a357985651aa2b71
bongjoo.seo <bongjoo.seo@samsung.com>
2024-09-04 11:55:18 +0900

    Init commint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
+const metadata = {
+  title: "Acme",
+  description: "Acme Company",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
